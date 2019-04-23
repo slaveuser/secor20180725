f219a3965f4f4f73335fe1ded4fa529700fe52c2
gmcpo <gmcpo@LAPTOP-2AD376QK>
2019-04-23 14:25:29 +0100

    Initial commit

---
+++ src/main/java/com/pinterest/secor/transformer/MessageTransformer.java
@@ -0,0 +1,19 @@
+package com.pinterest.secor.transformer;
+
+import com.pinterest.secor.message.Message;
+
+/**
+ * Message transformer And filter Interface
+ * 
+ * @author Ashish (ashu.impetus@gmail.com)
+ *
+ */
+public interface MessageTransformer {
+    /**
+     * Implement this method to add transformation logic at message level or filter out before
+     * dumping it into Amazon S3
+     * @param message message
+     * @return The transformed message or null if message should not be dumped to storage.
+     */
+    public Message transform(Message message);
+}
